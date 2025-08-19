ec7f64bed18ff1f9bc5b0526a12d6b163d3d97a0
karaik <3025067003@qq.com>
2025-08-19 17:53:46 +0800

    打包成exe，追加icon

--- src/main/java/com/karaik/scripteditor/EditorApplication.java
+++ src/main/java/com/karaik/scripteditor/EditorApplication.java
@@ -3,8 +3,10 @@ package com.karaik.scripteditor;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
+import java.net.URL;
 import java.util.Objects;
 
 public class EditorApplication extends Application {
@@ -13,6 +15,10 @@ public class EditorApplication extends Application {
         FXMLLoader loader = new FXMLLoader(getClass().getResource("/com/karaik/scripteditor/EditorView.fxml"));
         Scene scene = new Scene(loader.load(), 800, 600);
         scene.getStylesheets().add(Objects.requireNonNull(getClass().getResource("/com/karaik/scripteditor/css.css")).toExternalForm());
+        URL url = getClass().getResource("/images/head.png");
+        if (url != null) {
+            stage.getIcons().add(new Image(url.toExternalForm()));
+        }
         stage.setScene(scene);
         stage.setTitle("虫爱少女汉化文本编辑器");
         stage.show();
